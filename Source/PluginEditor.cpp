af8699eb76e752bf32a67b22d48b1e72c27e7f25
Sebastian Morales <sebastian.morales.e@ug.uchile.cl>
2025-12-30 00:06:29 -0300

    Dibujo de la Curva de Respuesta

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -40,8 +40,79 @@ SimpleEQAudioProcessorEditor::~SimpleEQAudioProcessorEditor()
 //==============================================================================
 void SimpleEQAudioProcessorEditor::paint (juce::Graphics& g)
 {
+	using namespace juce;
     // (Our component is opaque, so we must completely fill the background with a solid colour)
-    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
+    g.fillAll (Colours::black.brighter(0.1));
+
+	auto bounds = getLocalBounds();
+	auto responseArea = bounds.removeFromTop(bounds.getHeight() * 0.33);
+	auto w = responseArea.getWidth();
+
+	auto& lowcut = monoChain.get<ChainPositions::LowCut>();
+	auto& peak = monoChain.get<ChainPositions::Peak>();
+	auto& highcut = monoChain.get<ChainPositions::HighCut>();
+
+	auto sampleRate = audioProcessor.getSampleRate();
+
+	// En este vector se guardaran las magnitudes de cada frecuencia
+	// Necesitamos una magnitud por cada pixel de ancho en la zona de respuesta
+	std::vector<double> mags;
+	mags.resize(w);
+
+	// Ahora, iteramos por cada pixel de ancho, y calculamos la magnitud para cada frecuencia
+	for (int i = 0; i < w; ++i) {
+		// Convertimos la posicion x (i) en una frecuencia logaritmica entre 20Hz y 20kHz
+		auto freq = mapToLog10(double(i) / double(w), 20.0, 20000.0);
+
+		// Empezamos con una magnitud de 1.0 (0 dB)
+		double mag = 1.0;
+
+		if(!monoChain.isBypassed<ChainPositions::LowCut>())
+			mag *= peak.coefficients->getMagnitudeForFrequency(freq, sampleRate);
+
+		if(!lowcut.isBypassed<0>())
+			mag *= lowcut.get<0>().coefficients->getMagnitudeForFrequency(freq, sampleRate);
+		if (!lowcut.isBypassed<1>())
+			mag *= lowcut.get<1>().coefficients->getMagnitudeForFrequency(freq, sampleRate);
+		if (!lowcut.isBypassed<2>())
+			mag *= lowcut.get<2>().coefficients->getMagnitudeForFrequency(freq, sampleRate);
+		if (!lowcut.isBypassed<3>())
+			mag *= lowcut.get<3>().coefficients->getMagnitudeForFrequency(freq, sampleRate);
+
+		if (!highcut.isBypassed<0>())
+			mag *= highcut.get<0>().coefficients->getMagnitudeForFrequency(freq, sampleRate);
+		if (!highcut.isBypassed<1>())
+			mag *= highcut.get<1>().coefficients->getMagnitudeForFrequency(freq, sampleRate);
+		if (!highcut.isBypassed<2>())
+			mag *= highcut.get<2>().coefficients->getMagnitudeForFrequency(freq, sampleRate);
+		if (!highcut.isBypassed<3>())
+			mag *= highcut.get<3>().coefficients->getMagnitudeForFrequency(freq, sampleRate);
+
+		// Guardamos la magnitud en el vector
+		mags[i] = Decibels::gainToDecibels(mag);
+	}
+
+	// Ahora dibujamos la respuesta en frecuencia
+	Path responseCurve;
+
+	const double outputMin = responseArea.getBottom();
+	const double outputMax = responseArea.getY();
+	auto map = [outputMin, outputMax](double input) 
+	{
+		return jmap(input, -24.0, 24.0, outputMin, outputMax);
+	};
+
+	responseCurve.startNewSubPath(responseArea.getX(), map(mags.front()));
+
+	for(size_t i = 1; i < mags.size(); ++i) {
+		responseCurve.lineTo(responseArea.getX() + i, map(mags[i]));
+	}
+
+	g.setColour(Colours::rebeccapurple);
+	g.drawRoundedRectangle(responseArea.toFloat(), 4.0f, 3.0f);
+
+	g.setColour(Colours::white);
+	g.strokePath(responseCurve, PathStrokeType(2.0f));
 }
 
 void SimpleEQAudioProcessorEditor::resized()
